493109ebf58a7cd654bc00c85c3491722533a4e5
Djura92 <38479326+Djura92@users.noreply.github.com>
2022-11-24 11:26:45 +0100

    Fundamentals 1

---
+++ assigments.js
@@ -0,0 +1,91 @@
+/*
+
+
+let country = 'Serbia';
+let continent = 'Europe'
+let population = '6.908.000'
+
+console.log("Continent " + continent)
+console.log("Country " + country)
+console.log("Population " + population)
+
+
+*/
+
+
+/*
+const scoreDolphins = (97 + 112 + 81) / 3;
+const scoreKoalas = (109 + 95 + 86) / 3;
+
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log("Dolphins win the trophy");
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+    console.log("Koalas win the trophy");
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log("Both win the trophy");
+} else {
+    console.log("No one wins the trophy")
+}
+*/
+
+//SWITCH
+// with if else
+/*
+const day = 'thursday';
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code exmples');
+} else if ('friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
+}
+*/
+
+
+/////////////////////////
+
+///////////////////////     CODING CHALENGE #4 ////////////////////////////////
+/*
+// if else
+let tip;
+const bill = 430;
+let total;
+if (bill >= 50 && bill <= 300) {
+    total = bill * 1.15;
+    tip = total - bill;
+    console.log(`The bill was ${bill}, tip was ${tip} and the total value is ${total}`);
+} else {
+    total = bill * 1.2;
+    tip = total - bill;
+    console.log(`The bill was ${bill}, tip was ${tip} and the total value is ${total}`);
+}
+
+// ternary
+
+const tip2 = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, tip was ${tip2} and the total value is ${bill + tip2}`);
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+

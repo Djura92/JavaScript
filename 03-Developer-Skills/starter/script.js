1708bb5ce885de5e33390bd47016afc3317ef2ce
Djura92 <38479326+Djura92@users.noreply.github.com>
2022-12-12 13:09:49 +0100

    Add files via upload

---
+++ 03-Developer-Skills/starter/script.js
@@ -0,0 +1,43 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+// const x = 23;
+
+// console.log(`asdasd`);
+// console.log('asdasd');
+
+// const calcAge = birthYear => 2037 - birthYear;
+
+// console.log(calcAge(1991));
+
+//------------------------- PRACTICE -------------------------//
+
+// const temperatures = [3, -2, -6, -1, `error`, 9, 13, 17, 15, 14, 9, 5];
+
+// const calcTempAmplitute = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+
+//     if (typeof curTemp !== `number`) continue;
+
+//     if (curTemp < min) min = curTemp;
+
+//     if (curTemp > max) max = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+
+// //calcTempAmplitute([3, 7, 4, 1, 8]);
+
+// const amplitude = calcTempAmplitute(temperatures);
+// console.log(amplitude);
+
+//------------------------------------------------------------//
+//------------------------- PRACTICE -------------------------//
+//------------------------------------------------------------//
+//------------------------- PRACTICE -------------------------//
+//------------------------------------------------------------//
